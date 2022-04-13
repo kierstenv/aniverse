fca3e3ab4439e1a31faec7af5cb0b33cba758d09
kierstenv <kierstenvicknair22@gmail.com>
2022-04-12 19:32:04 -0500

    connect mysql

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,15 @@
-const Sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
 
 require('dotenv').config();
 
-//trying to push to heroku
+// trying to push to heroku
 // create connection to our db 
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
- const sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  const sequelize = new Sequelize(
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PW,
