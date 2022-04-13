2878707848e6b9faa5ae7eb74d25288ac91137fd
kierstenv <kierstenvicknair22@gmail.com>
2022-04-12 19:28:47 -0500

    connect mysql

--- config/connection.js
+++ config/connection.js
@@ -7,17 +7,18 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+ const sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(
+  const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PW,
     {
-      host: 'My mySQL server',
+      host: 'localhost',
       dialect: 'mysql',
       port: 3306,
     },
   );
 }
+
 module.exports = sequelize;
